f25a897ea5ca5cbc994e8e37f8f6149493578461
uchenna333@yahoo.com <uchenna333@yahoo.com>
2022-12-27 12:24:42 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - entry point
-* return: 0 (shows the program is succesful)
+* return: 0 (shows the program is successful)
 */
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
