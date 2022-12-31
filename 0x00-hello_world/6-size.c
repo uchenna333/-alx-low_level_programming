17b0b6120b0b9e9f07e5a0d192b2bb3501b7368b
uchenna333@yahoo.com <uchenna333@yahoo.com>
2022-12-30 22:18:52 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this program will print out the various sizes of data types
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long long byte: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+return (0);
+}
