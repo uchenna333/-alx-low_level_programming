25cb425d4ccd7976d09f062938e138c533690cf7
uchenna333@yahoo.com <uchenna333@yahoo.com>
2022-12-31 22:22:01 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ char a;
 int b;
 long d;
 long long e;
-float d;
+float g;
 printf("Size of a char:%lu byte(s)\n", sizeof(a));
 printf("Size of an int:%lu byte(s)\n", sizeof(b));
 printf("Size of a long int:%lu byte(s)\n", sizeof(d));
-printf("Size of a long long byte:%lu byte(s)\n", sizeof(e))
-printf("Size of a float:%lu byte(s)\n", sizeof(d));
+printf("Size of a long long byte:%lu byte(s)\n", sizeof(e));
+printf("Size of a float:%lu byte(s)\n", sizeof(g));
 return (0);
 }
